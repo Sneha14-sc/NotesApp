b9859f1128c61e70c75e754686b4ad2633d7c38d
Sneha Choudhari <67182479+Sneha14-sc@users.noreply.github.com>
2022-09-21 13:01:56 +0530

    added src

---
+++ backend/NestJs/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { NotesModule } from './notes/notes.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Notes } from './notes/notes.entity';
+import { NotesController } from './notes/notes.controller';
+import { NotesService } from './notes/notes.service';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'Sneha1415',
+    database: 'NewDB',
+    synchronize: true,
+    entities: [Notes],
+  }),NotesModule],
+  controllers: [AppController, NotesController],
+  providers: [AppService, NotesService],
+})
+export class AppModule {
+
+}
